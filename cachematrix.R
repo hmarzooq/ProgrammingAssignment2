9bc11e7d36b69301956d7ca96aef9128ab975683
Syed Hassan Marzooq Naqvi <18892622@curtin.edu.au>
2016-09-30 23:33:16 +0800

    Update repo with week3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,40 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(a = matrix()) {
+        invr <- NULL
+        set <- function(n) {
+                a <<- n
+                invr <<- NULL
+        }
+        get <- function() a
+        set_invr <- function(inverse) invr <<- inverse
+        get_invr <- function() invr
+        list(set=set, get=get, set_invr=set_invr, get_invr=get_invr)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(a, ...) {
         ## Return a matrix that is the inverse of 'x'
+        invr <- a$get_invr()
+        if(!is.null(invr)) {
+                message("Retreiving data from cache.")
+                return(invr)
+        }
+        data <- a$get()
+        invr <- solve(data)
+        a$set_invr(invr)
+        return(invr)
 }
+
+a = rbind(c(1, -1/4), c(-1/4, 1))
+m = makeCacheMatrix(a)
+m$get()
+
+
+cacheSolve(m)
+
+
+cacheSolve(m)
